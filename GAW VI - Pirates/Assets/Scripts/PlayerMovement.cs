21015eb82444eeee5dfe04718a2af31c3514453a
MikeCooke19 <mikethomascooke@gmail.com>
2023-04-25 14:57:27 +0100

    Player Movement and SkyBox

---
+++ GAW VI - Pirates/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //For Movement
+    public float moveSpeed;
+    public Transform orientation;
+    float horizontalInput;
+    float verticalInput;
+    public float groundDrag = 5f;
+    Vector3 moveDirection;
+    Rigidbody rb;
+    public bool onGround = true;
+    public float jumpForce = 5f;
+    public float wallRunSpeed;
+    public bool wallrunning;
+
+
+    //For Camera
+    public Transform PlayerCamera;
+    public Vector2 sens;
+
+    private Vector2 XYRotation;
+
+
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        //Makes mouse invisible
+        //Cursor.visible = false;
+
+        wallRunSpeed = moveSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+
+    }
+
+    void Update()
+    {
+
+        //All Movement
+        MyInput();
+        SpeedControl();
+
+        //checks if can jump
+        if (onGround == true)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                onGround = false;
+
+            }
+        }
+
+        if (onGround == true)
+        {
+            rb.drag = groundDrag;
+        }
+
+        if (onGround == false)
+        {
+            rb.drag = 0f;
+        }
+
+        //All For Camera
+
+        Vector2 MouseInput = new Vector2
+        {
+            x = Input.GetAxisRaw("Mouse X"),
+            y = Input.GetAxisRaw("Mouse Y")
+
+        };
+
+
+        XYRotation.x -= MouseInput.y * sens.y;
+        XYRotation.y += MouseInput.x * sens.x;
+
+        XYRotation.x = Mathf.Clamp(XYRotation.x, -90f, 90f);
+        transform.eulerAngles = new Vector3(0f, XYRotation.y, 0f);
+        PlayerCamera.localEulerAngles = new Vector3(XYRotation.x, 0f, 0f);
+
+    }
+
+    void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+    }
+
+    //movement
+    void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+
+    //Jump
+    void OnCollisionEnter(Collision collision)
+    {
+        //For Jump
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+
+        }
+
+        //For Reset
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+
+            //message.SetActive(true);
+            //button.SetActive(true);
+            //Cursor.visible = true;
+            //sens = sens - sens;
+            //moveSpeed = moveSpeed - moveSpeed;
+
+
+
+
+
+
+        }
+        else
+        {
+            //message.SetActive(false);
+            //button.SetActive(false);
+        }
+
+    }
+
+    void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            rb.drag = 0f;
+            onGround = false;
+        }
+    }
+}
